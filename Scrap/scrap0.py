8be9b9ef529791500219749305cad02799d2de95
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-07-29 13:57:42 +0545

    Update scrap0.py

--- Scrap/scrap0.py
+++ Scrap/scrap0.py
@@ -1,3 +1,4 @@
+from re import I
 import requests
 from bs4 import BeautifulSoup
 def navigable():
@@ -15,4 +16,50 @@ def finding():
   return soup.find("h4",{"class":"pull-right price"})
   price=soup.find("h4",{"class":"pull-right price"})
   return (price.string)
-print(finding())
+#print(finding())
+#GEEING THE DESCRIPTION OF THE FIRST ELEMENT OF THE DIV TAGS 
+def findingdescription():
+  url="https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+  r=requests.get(url)
+  soup=BeautifulSoup(r.text,"html.parser")
+  desc=soup.find("p",{"class","description"})
+  return desc.string
+#  return soup.find("p",{"class","description"})
+
+#print(findingdescription())
+
+def findingAllDescription():
+  url="https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+  r=requests.get(url)
+  soup=BeautifulSoup(r.text,"html.parser")
+  prices=soup.find_all("h4",class_="pull-right price")
+  #return prices
+  #return len(prices)
+  for i in prices:
+    return i
+#print(findingAllDescription())
+#  printing all the prices
+def findingAllDescription1():
+    url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text,"html.parser")
+    prices = soup.find_all("h4", {"class":"pull-right price"})
+    return len(prices)
+    return prices
+    for price1 in prices:
+      return price1
+    for price in prices:
+        print(price.text)  # print the text inside the HTML tag
+    return prices[3]
+#print(findingAllDescription1())
+def findingAllDescription2():
+  url="https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+  r=requests.get(url)
+  soup=BeautifulSoup(r.text,"html.parser")
+  description=soup.find_all("p",class_="description")
+  #return len(description)
+  #return description
+  for descriptiononly in description:
+    print(descriptiononly.text)
+#print(findingAllDescription2())
+#

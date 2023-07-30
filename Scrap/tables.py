0bb94e2d9e9767adbd65e88ff1f945d0af09b27e
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-07-30 09:10:24 +0545

    Update tables.py

--- Scrap/tables.py
+++ Scrap/tables.py
@@ -36,5 +36,33 @@ def third():
         headers_text.append(headersitr)
     df = pd.DataFrame(columns=headers_text)  # Corrected here
     return df
-
-print(third())
+#GETING ALL THE TABLE DATA 
+def fourth():
+  url="https://ticker.finology.in/"
+  r=requests.get(url)
+  soup=BeautifulSoup(r.text,"html.parser")
+  table=soup.find("table",class_="table table-sm table-hover screenertable")
+  rows=table.find_all("tr")
+  check=[]
+  for hello in rows[1:]:
+    dank=hello.text
+    check.append(dank)
+ #return check
+  check1=[]
+  for newli in check:
+    newli2=newli.replace("\n","  ")
+    check1.append(newli2)
+  for item in check1:
+        print(item)
+  '''
+  for i in rows[1:]:
+    data=i.find_all("td")
+    row=[tr.text for tr in data]
+    return row
+  ''' 
+ # df=pd.DataFrame()
+  df = pd.DataFrame(check1, columns=['Data'])
+    
+    # Save DataFrame to csv
+  df.to_csv('data.csv', index=False)
+print(fourth())

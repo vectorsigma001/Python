9f90f0ce890dbe0ddb5997163d0c7cff5a90927c
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-07-30 19:40:47 +0545

    Update Multipage.py

--- Scrap/Multipage.py
+++ Scrap/Multipage.py
@@ -13,4 +13,20 @@ def scrapingMobilePhones():
   
 
 scrapingMobilePhones()
-  1
+
+#WHEN YOU HAVE A CERTAIN CHANGES IN DIFFERENT PAGE YOU CAN MANIPULATE IT IN THIS WAY
+#SCRAPING MOBILE PHONES UNDER 500000
+def scrapingMobilePhones():
+  for i in range(1,11):
+    url="https://www.flipkart.com/search?q=mobiles+under+50000&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&as-pos=1&as-type=HISTORY&page="+str(i)
+    r=requests.get(url)
+    soup=BeautifulSoup(r.text,"html.parser")
+    np=soup.find("a",class_="_1LKTO3").get("href")
+    cnp="https://www.flipkart.com"+np
+    print(url)
+      #url=cnp
+      #r=requests.get(url)
+      #soup=BeautifulSoup(r.text,"html.parser")
+  
+
+scrapingMobilePhones()

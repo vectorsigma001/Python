01af96b98efbfc6ec9fc2d88374f3bfe8611b22c
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-07-31 20:09:53 +0545

    Update Multipage.py

--- Scrap/Multipage.py
+++ Scrap/Multipage.py
@@ -30,3 +30,51 @@ def scrapingMobilePhones():
   
 
 scrapingMobilePhones()
+
+#SCRAPING FLIPKART SIGNLE PAGE 
+class Multiweb:
+  url="https://www.flipkart.com/search?q=mobiles+under+50000&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&as-pos=1&as-type=HISTORY&page=1"
+  names=[]
+  prices=[]
+  descriptions=[]
+  reviews=[]
+  def check(self):   
+    r=requests.get(self.url)
+    self.soup=BeautifulSoup(r.text,"html.parser")
+    
+  def name(self):
+    name1=self.soup.find_all("div",class_="_4rR01T")
+    for namecheck in name1:
+      namecheck1=namecheck.text
+      self.names.append(namecheck1)
+      
+  def price(self):
+    price1=self.soup.find_all("div",class_="_30jeq3 _1_WHN1")
+    for pricecheck in price1:
+      pricecheck1=pricecheck.text
+      self.prices.append(pricecheck1)
+      
+  def description(self):
+    description1=self.soup.find_all("ul",class_="_1xgFaf")
+    for descriptioncheck in description1:
+      descriptioncheck1=descriptioncheck.text
+      self.descriptions.append(descriptioncheck1)
+     
+  def review(self):
+    review1=self.soup.find_all("div",class_="fMghEO")
+    for reviewcheck in review1:
+      reviewcheck1=reviewcheck.text
+      self.reviews.append(reviewcheck1)
+    
+    
+  def dataframe(self):
+    df=pd.DataFrame({"Name":self.names,"Price":self.prices,"Description":self.descriptions,"Reviews":self.reviews})
+    df.to_csv("MobileUnder5k.csv",index=False)
+     
+object=Multiweb()
+object.check()
+object.name()
+object.price()
+object.description()
+object.review()
+object.dataframe()
